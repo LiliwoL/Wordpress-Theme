ba740c921c587e453fa42c00c638d6ee1f2b8909
Nicolas COSNEAU <ncosneau@campus-eni.fr>
2022-10-26 15:06:45 +0200

    Sidebar

--- functions.php
+++ functions.php
@@ -34,3 +34,57 @@ function eni_menus()
 }
 
 add_action ('after_setup_theme', 'eni_menus');
+
+
+// ************************************************
+
+/**
+ * Pour afficher les derniers articles
+ */
+function display_last_articles()
+{
+	echo '<h2>Liste des derniers articles</h2>';
+
+        // Afficher la liste des contenus (posts)
+        // Instance de WP_Query
+        // https://developer.wordpress.org/reference/classes/wp_query/#order-orderby-parameters
+        $args = array(
+            'order'             => 'ASC',
+            'orderby'           => 'date',
+            'posts_per_page'    => 5
+        );
+        $query = new WP_Query( $args );
+        
+        // Résultats?
+        if ($query->have_posts())
+        {        
+            // Parcours des contenus
+            while ( $query->have_posts() )
+            {
+                // Récupère le post en cours
+                $query->the_post();
+
+                // https://getbootstrap.com/docs/5.2/components/card/
+
+                echo '<div class="card mb-3" style="width: 18rem;">';
+                    // Lien
+                    echo '<a href="' . get_the_permalink() . '" title="' . get_the_title() . '">';
+
+                        // Affiche
+                        echo '<img src="' . get_the_post_thumbnail_url() . '" 
+                            class="card-img-top" alt="' . get_the_title() . '">';
+
+                        echo '<div class="card-body">';
+                            // Titre
+                            echo '<h5 class="card-title">' . get_the_title() . '</h5>';
+                        echo '</div>';
+                                
+                    // Lien
+                    echo '</a>';
+                echo '</div>';
+            }
+        
+        }else{
+            echo "Aucun contenu à afficher";
+        }
+}
--- sidebar.php
+++ sidebar.php
@@ -1,48 +1,8 @@
 <div class="col-4">
+
     <?php
-
-        // Afficher la liste des contenus (posts)
-        // Instance de WP_Query
-        // https://developer.wordpress.org/reference/classes/wp_query/#order-orderby-parameters
-        $args = array(
-            'order'             => 'ASC',
-            'orderby'           => 'date',
-            'posts_per_page'    => 5
-        );
-        $query = new WP_Query( $args );
-        
-        // Résultats?
-        if ($query->have_posts())
-        {        
-            // Parcours des contenus
-            while ( $query->have_posts() )
-            {
-                // Récupère le post en cours
-                $query->the_post();
-
-                // https://getbootstrap.com/docs/5.2/components/card/
-
-                echo '<div class="card mb-3" style="width: 18rem;">';
-                    // Lien
-                    echo '<a href="' . get_the_permalink() . '" title="' . get_the_title() . '">';
-
-                        // Affiche
-                        echo '<img src="' . get_the_post_thumbnail_url() . '" 
-                            class="card-img-top" alt="' . get_the_title() . '">';
-
-                        echo '<div class="card-body">';
-                            // Titre
-                            echo '<h5 class="card-title">' . get_the_title() . '</h5>';
-                        echo '</div>';
-                                
-                    // Lien
-                    echo '</a>';
-                echo '</div>';
-            }
-        
-        }else{
-            echo "Aucun contenu à afficher";
-        }
+        // Appel de la fonction d'affichage des derniers articles
+        display_last_articles();
     ?>
 
 </div>
