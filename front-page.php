ffddec1fbc92740a62da7d6e6b6445bf76b2d21a
Nicolas COSNEAU <ncosneau@campus-eni.fr>
2022-10-26 15:34:03 +0200

    Sidebar

--- front-page.php
+++ front-page.php
@@ -1,15 +1,29 @@
 <?php
 
-// Affichage du header
-get_header();
+    // Affichage du header
+    get_header();
 
 ?>
 
-<div class="col-12">
-    CONTENU PAGE D'ACCUEIL
+<div class="col-8">
+    <?php
+        // La requête a déjà été faite
+
+        // Affichage du titre
+        echo '<h1>' . get_the_title() . '</h1>';
+
+        // Affichage de l'image mise en avant
+        // the_post_thumbnail();
+
+        // Affichage du contenu
+        the_content();
+    ?>
 </div>
 
 <?php
+
+// Affichage sidebar
+get_sidebar();
 
 
 // footer
