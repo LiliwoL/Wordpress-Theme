40367776faf85dd4d842153933a2956c1b6aef68
Nicolas COSNEAU <ncosneau@campus-eni.fr>
2022-10-27 17:16:13 +0200

    Sidebar

--- functions.php
+++ functions.php
@@ -302,3 +302,112 @@ function create_movie_type()
 
 }
 add_action( 'init', 'create_movie_type', 0 );
+
+// Register Custom Post Type
+function create_seance_type() {
+
+	$labels = array(
+		'name'                  => _x( 'Séances', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Séance', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Séances de films', 'text_domain' ),
+		'name_admin_bar'        => __( 'Séance de films', 'text_domain' ),
+		'archives'              => __( 'Archive des séances', 'text_domain' ),
+		'attributes'            => __( 'Attributs des séances', 'text_domain' ),
+		'parent_item_colon'     => __( 'Séance parente', 'text_domain' ),
+		'all_items'             => __( 'Toutes les séances', 'text_domain' ),
+		'add_new_item'          => __( 'Ajouter une séance', 'text_domain' ),
+		'add_new'               => __( 'Ajouter une séance', 'text_domain' ),
+		'new_item'              => __( 'Nouvelle séance', 'text_domain' ),
+		'edit_item'             => __( 'Modifier une séance', 'text_domain' ),
+		'update_item'           => __( 'Mettre à jour', 'text_domain' ),
+		'view_item'             => __( 'Voir', 'text_domain' ),
+		'view_items'            => __( 'Voir les séances', 'text_domain' ),
+		'search_items'          => __( 'Rechercher', 'text_domain' ),
+		'not_found'             => __( 'Séance non trouvée', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+		'items_list'            => __( 'Items list', 'text_domain' ),
+		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Séance', 'text_domain' ),
+		'description'           => __( 'Séances des films', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-calendar-alt',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => false,
+		'has_archive'           => false,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+		'show_in_rest'          => false,
+	);
+	register_post_type( 'seances', $args );
+
+}
+add_action( 'init', 'create_seance_type', 0 );
+
+
+// **********************************************
+
+// Hook pour modifier les colonnes affichées en backoffice pour le type de contenu seances
+add_filter( 'manage_seances_posts_columns', 'display_seances_posts_columns' );
+function display_seances_posts_columns( $columns ) 
+{
+	// Ajout de la colonne film
+  	$columns['Film'] = __( 'Film' );
+  	$columns['DateHeure'] = __( 'DateHeure' );
+
+	// On retire la colone titre
+	unset($columns['title']);
+
+  	return $columns;
+}
+
+// **********************************************
+
+// Hook pour afficher du contenu dans les colonnes ajoutées dans le hook précédent
+add_action( 'manage_seances_posts_custom_column', 'display_seances_posts_column', 10, 2);
+function display_seances_posts_column( $column, $post_id ) 
+{
+  // Film column
+  if ( 'Film' === $column ) {
+    // Récupération de l'id du film
+	$idFilm = get_field( 'film', $post_id);
+
+	echo get_the_post_thumbnail( $idFilm, array(80, 80) );
+  }
+
+  // Date et Heure column
+  if ( 'DateHeure' === $column ) {
+    echo get_field( 'date_et_heure_de_debut', $post_id);
+  }
+}
+
+// **********************************************
+
+// Hook d'activation du tri
+add_filter( 'manage_edit-seances_sortable_columns', 'seances_sortable_columns');
+function seances_sortable_columns( $columns ) {
+  
+	// Tri sur la colonne film
+	$columns['Film'] = 'film';
+
+	// Tri sur la colonne Date Heure
+	$columns['DateHeure'] = 'date_et_heure_de_debut';
+
+  	return $columns;
+}
--- single-movies.php
+++ single-movies.php
@@ -8,25 +8,79 @@
 <div class="col-10">
     <?php
         // Afficher le contenu du Film
-        
-        the_title();
-
-        the_content();
-
-        // Debug de tous les champs personnalisés
-        //var_dump( get_post_meta( get_the_ID() ) );
-
-        // Date de sortie
-        //echo '<span>' . get_post_meta( get_the_ID(), 'annee_de_sortie', true ) , '</span>';        
-        echo '<span>Année de sortie: ' . get_field( 'annee_de_sortie' ) . '</span>';
-
-        // Distribution
-        echo '<div>Distribution:<br/>' . get_field('distribution') . '</div>';
-
-        // Censure
-        // Attention, c'est un tableau!
-        echo '<div>Censure: ' . get_field('censure')[0] . '</div>';
     ?>
+
+    <div class="row">
+        <div class="col-4">
+            <div class="image">
+                <figure class="poster">
+                    <img alt="<?= get_the_title(); ?>" 
+                        src="<?= get_the_post_thumbnail_url(); ?>"
+                        class="w-100">
+                </figure>
+            </div>
+        </div>
+
+        <div class="col-8">
+            <?php
+                the_title();
+
+                the_content();
+        
+                // Debug de tous les champs personnalisés
+                //var_dump( get_post_meta( get_the_ID() ) );
+        
+                // Date de sortie
+                //echo '<span>' . get_post_meta( get_the_ID(), 'annee_de_sortie', true ) , '</span>';        
+                echo '<span>Année de sortie: ' . get_field( 'annee_de_sortie' ) . '</span>';
+        
+                // Distribution
+                echo '<div>Distribution:<br/>' . get_field('distribution') . '</div>';
+        
+                // Censure
+                // Attention, c'est un tableau!
+                echo '<div>Censure: ' . get_field('censure')[0] . '</div>';
+            ?>
+
+            <hr/>
+
+            <div>
+                <h4>Séances à venir:</h4>
+                <?php
+                    // https://developer.wordpress.org/reference/classes/wp_query/#custom-field-post-meta-parameters
+
+                    // Arguments
+                    $args = array(
+                        'post_type' => 'seances',
+                        'posts_per_page' => 5,
+                        
+                        // Critère de sélection pour l'id du film affiché
+                        'meta_key'   => 'film',
+	                    'meta_value' => get_the_ID()
+                    );
+
+                    // Requete
+                    $query = new WP_Query( $args );
+
+                    // Résultats?
+                    if ($query->have_posts())
+                    {        
+                        // Parcours des contenus
+                        while ( $query->have_posts() )
+                        {
+                            // Récupère le post en cours
+                            $query->the_post();
+
+                            // Afficher l'heure
+                            echo get_field('date_et_heure_de_debut', get_the_ID());
+
+                            echo "<br>";
+                        }
+                    }
+                ?>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php
